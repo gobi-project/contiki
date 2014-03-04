d695c9cf54adbe174b14b2eca09128cd73ce1840
Bastian <hbastian@tzi.de>
2014-03-04 15:54:48 +0100

    hibsave working ... destination address missing

--- platform/econotag/apps/hibsave/hibsave.c
+++ platform/econotag/apps/hibsave/hibsave.c
@@ -9,9 +9,15 @@
 
 /* nvm-write */
 #define WRITE_NBYTES 256
-#define WRITE_ADDR 0x1d000
+#define WRITE_ADDR 0x1D000
 
-static void*		g_SAVEDUMP[WRITE_NBYTES];
+#if DEBUG
+    #define PRINTF(...) printf(__VA_ARGS__)
+#else
+    #define PRINTF(...)
+#endif
+
+static uint32_t		g_SAVEDUMP[WRITE_NBYTES];
 static uint8_t		g_SAVEPTR = 0;
 static nvmType_t	g_VNMTYPE = 0;
 
@@ -22,10 +28,10 @@ void hibs_init()
 	vreg_init();
 
 	err = nvm_detect(NVM_INTERFACE, &g_VNMTYPE);	
-	printf("nvm_detect returned: 0x%02x type is: 0x%08x\r\n", err, (unsigned int)g_VNMTYPE);
+	PRINTF("nvm_detect returned: 0x%02x type is: 0x%08x\r\n", err, (unsigned int)g_VNMTYPE);
 		
 	err = nvm_read(NVM_INTERFACE, g_VNMTYPE, (uint8_t *)g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
-	printf("nvm_read returned: 0x%02x\r\n", err);
+	PRINTF("nvm_read returned: 0x%02x\r\n", err);
 }
 
 uint32_t hibs_save(void* memory, uint8_t size)
@@ -34,7 +40,7 @@ uint32_t hibs_save(void* memory, uint8_t size)
 	uint32_t dest = g_SAVEPTR;
 	while(size--)
 	{
-		((uint32_t*)g_SAVEDUMP)[g_SAVEPTR++] = ((uint32_t*)memory)[ptr++];
+		g_SAVEDUMP[g_SAVEPTR++] = ((uint32_t*)memory)[ptr++];
 	}
 	return dest;
 }
@@ -44,7 +50,7 @@ void hibs_load(void* memory, uint8_t size, uint32_t dest)
 	uint8_t ptr = 0;
 	while(size--)
 	{
-		((uint32_t*)memory)[ptr++] = ((uint32_t*)g_SAVEDUMP)[dest++];
+		((uint32_t*)memory)[ptr++] = g_SAVEDUMP[dest++];
 	}
 }
 
@@ -53,5 +59,14 @@ void hibs_finit()
 	nvmErr_t err;
 
 	err = nvm_verify(NVM_INTERFACE, g_VNMTYPE, g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
-	printf("nvm_verify returned: 0x%02x\r\n", err);
+	PRINTF("nvm_verify returned: 0x%02x\r\n", err);
+	
+	if (err) // Changes happend -> overwrite
+	{
+		err = nvm_erase(NVM_INTERFACE, g_VNMTYPE, 1 << WRITE_ADDR/4096);
+		PRINTF("nvm_erase returned: 0x%02x\r\n", err);
+
+		err = nvm_write(NVM_INTERFACE, g_VNMTYPE, (uint8_t *)g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
+		PRINTF("nvm_write returned: 0x%02x\r\n", err);
+	}
 }
