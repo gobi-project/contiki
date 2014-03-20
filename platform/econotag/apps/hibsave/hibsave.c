d2b295072af1fc716fa8226bfd451687bb21b3a6
Bastian <hbastian@tzi.de>
2014-03-20 11:42:31 +0100

    fixed and updated memsave

--- platform/econotag/apps/hibsave/hibsave.c
+++ platform/econotag/apps/hibsave/hibsave.c
@@ -9,15 +9,24 @@
 
 /* nvm-write */
 #define WRITE_NBYTES 256
-#define WRITE_ADDR 0x1D000
+#define WRITE_ADDR 0x07c68 //0x1D000 //
 
 #if DEBUG
     #define PRINTF(...) printf(__VA_ARGS__)
 #else
-    #define PRINTF(...)
+    #define PRINTF(...) printf(__VA_ARGS__)
 #endif
 
-static uint32_t		g_SAVEDUMP[WRITE_NBYTES];
+typedef struct __attribute__((packed))
+{
+	uint32_t	FLASHADDRESS;
+	uint32_t	RAMADDRESS;
+	uint32_t	LENGTH;
+} hibdata;
+
+//0x00407c68      0x400
+static uint32_t		__persistent__							g_SAVEDUMP[WRITE_NBYTES];
+static hibdata		__attribute__ ((section ("HIBDATA")))	g_HIBDATA = {0};
 static uint8_t		g_SAVEPTR = 0;
 static nvmType_t	g_VNMTYPE = 0;
 
@@ -26,12 +35,17 @@ void hibs_init()
 	nvmErr_t err;
 
 	vreg_init();
-
+	
 	err = nvm_detect(NVM_INTERFACE, &g_VNMTYPE);	
 	PRINTF("nvm_detect returned: 0x%02x type is: 0x%08x\r\n", err, (unsigned int)g_VNMTYPE);
 		
-	err = nvm_read(NVM_INTERFACE, g_VNMTYPE, (uint8_t *)g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
+	// Read: flash -> ram
+	err = nvm_read(NVM_INTERFACE, g_VNMTYPE, (uint8_t*)g_HIBDATA.RAMADDRESS, g_HIBDATA.FLASHADDRESS, g_HIBDATA.LENGTH);
 	PRINTF("nvm_read returned: 0x%02x\r\n", err);
+
+	g_SAVEDUMP[1]++;	
+	
+	printf("%d\n", g_SAVEDUMP[1]);
 }
 
 uint32_t hibs_save(void* memory, uint8_t size)
@@ -57,16 +71,21 @@ void hibs_load(void* memory, uint8_t size, uint32_t dest)
 void hibs_finit()
 {
 	nvmErr_t err;
-
-	err = nvm_verify(NVM_INTERFACE, g_VNMTYPE, g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
-	PRINTF("nvm_verify returned: 0x%02x\r\n", err);
-	
-	if (err) // Changes happend -> overwrite
+	if (g_HIBDATA.LENGTH)
 	{
-		err = nvm_erase(NVM_INTERFACE, g_VNMTYPE, 1 << WRITE_ADDR/4096);
-		PRINTF("nvm_erase returned: 0x%02x\r\n", err);
+		// Test: ram changed?
+		err = nvm_verify(NVM_INTERFACE, g_VNMTYPE, (uint8_t*)g_HIBDATA.RAMADDRESS, g_HIBDATA.FLASHADDRESS, g_HIBDATA.LENGTH);
+		PRINTF("nvm_verify returned: 0x%02x\r\n", err);
+		
+		if (err) // Changes happend -> overwrite
+		{
+			// Erase: clear block
+			err = nvm_erase(NVM_INTERFACE, g_VNMTYPE, 1 << g_HIBDATA.FLASHADDRESS/0x1000);
+			PRINTF("nvm_erase returned: 0x%02x\r\n", err);
 
-		err = nvm_write(NVM_INTERFACE, g_VNMTYPE, (uint8_t *)g_SAVEDUMP, WRITE_ADDR, sizeof(g_SAVEDUMP));
-		PRINTF("nvm_write returned: 0x%02x\r\n", err);
+			// Write: ram -> flash, write complete blocks
+			err = nvm_write(NVM_INTERFACE, g_VNMTYPE, (uint8_t*)(g_HIBDATA.RAMADDRESS & 0xFFFFF000), (g_HIBDATA.FLASHADDRESS & 0xFFFFF000), (g_HIBDATA.LENGTH & 0xFFFFF000) + 0x1000);
+			PRINTF("nvm_write returned: 0x%02x\r\n", err);
+		}
 	}
 }
--- platform/econotag/apps/hibsave/hibsave.h
+++ platform/econotag/apps/hibsave/hibsave.h
@@ -9,6 +9,19 @@
 #define __HIBSAVE_H__
 
 #include <stdint.h>
+#include "memb.h"
+
+#define 	__persistent__ __attribute__ ((section ("SAVEDATA")))
+
+#ifdef MEMB
+#undef MEMB
+#define MEMB(name, structure, num) \
+        static char __persistent__ CC_CONCAT(name,_memb_count)[num]; \
+        static structure __persistent__ CC_CONCAT(name,_memb_mem)[num]; \
+        static struct memb__persistent__ name = {sizeof(structure), num, \
+                                          CC_CONCAT(name,_memb_count), \
+                                          (void *)CC_CONCAT(name,_memb_mem)}
+#endif
 
 void 		hibs_init();
 
